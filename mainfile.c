837f0d077526c1f8df7729f958f49d0c71b01e7b
penicilllia <penicilllia@gmail.com>
2020-06-04 08:42:53 +0500

    Makefile was changed

--- mainfile.c
+++ mainfile.c
@@ -8,18 +8,24 @@ int main()
 	int a;
 	printf("\nEnter the num: ");
 	scanf("%d", &a);
-	int lennum = lenOfNumber(a);
-	int maxnum = maxNumber(a);
-	printf("\nLen of numb is: %d", lennum);
-	printf("\nMax num is: %d\n", maxnum);
+	//int lennum = lenOfNumber(a);
+	//int maxnum = maxNumber(a);
+	//printf("\nLen of numb is: %d", lennum);
+	//printf("\nMax num is: %d\n", maxnum);
 	float k = 0;
 	printf("\n\nHell World\n\n");
+	int r[2];
+	for (int i = 0; i<10; ++i)
+	{
+		r[i] = i;
+		printf("\n%d", r[i]);
+	}
 	while(k < 1)
 	{
 		k -= 0.01;
 		char* c = malloc(sizeof(10));
-		c = "hello world";
-		printf("%s\n", c);
+		//c = "hello world";
+		//printf("%s\n", c);
 	}
 
 	return 0;
--- makefile
+++ makefile
@@ -2,19 +2,18 @@ TARGET=mainfl
 FIRSTLIB=libmy1.a
 SECLIB=libmy2.so
 MYMAIN=mainfile
-CFlAGS=-g
 all: $(TARGET)
 $(TARGET): $(MYMAIN).o $(FIRSTLIB) $(SECLIB)
-	gcc -o $(TARGET) *.o libmy*
+	gcc -g -Wall -o $(TARGET) *.o libmy*
 	export LD_LIBRARY_PATH='./'
-	./$(TARGET)
+	gdb ./$(TARGET)
 $(MYMAIN).o: $(MYMAIN).c
-	gcc -c $(MYMAIN).c
+	gcc -g -Wall -c $(MYMAIN).c
 libmy1.a: file1.o
-	gcc -c file1.c
+	gcc -g -Wall -c file1.c
 	ar r $(FIRSTLIB)
 libmy2.so: file2.o
-	gcc -c -fPIC file2.c
+	gcc -g -Wall -c -fPIC file2.c
 	gcc -shared -o $(SECLIB) file2.o
 clean:
 	rm -f *.o
